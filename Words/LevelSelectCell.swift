7c6cd141278ac4a8843a86da3ef153e1dddf9e10
Nir Bitton <nir.bitton@intel.com>
2017-10-22 21:46:54 +0300

    Level select

---
+++ Words/LevelSelectCell.swift
@@ -0,0 +1,13 @@
+//
+//  LevelSelectCell.swift
+//  Words
+//
+//  Created by Nir Bitton on 21/10/2017.
+//  Copyright © 2017 Nir. All rights reserved.
+//
+
+import UIKit
+
+class LevelSelectCell: UICollectionViewCell {
+    
+}
---
+++ Words/WordSelectCell.swift
@@ -0,0 +1,13 @@
+//
+//  WordSelectCell.swift
+//  Words
+//
+//  Created by Nir Bitton on 22/10/2017.
+//  Copyright © 2017 Nir. All rights reserved.
+//
+
+import UIKit
+
+class WordSelectCell: UICollectionViewCell {
+    
+}
