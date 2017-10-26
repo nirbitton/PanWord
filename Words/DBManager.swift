03976b6aa111bf4f04f4a96a8e0e990385dd8143
Nir Bitton <nir.bitton@intel.com>
2017-10-27 01:13:11 +0300

    add adMob

---
+++ Words/DBManager.swift
@@ -0,0 +1,13 @@
+//
+//  DBManager.swift
+//  Words
+//
+//  Created by Nir Bitton on 23/10/2017.
+//  Copyright © 2017 Nir. All rights reserved.
+//
+
+import UIKit
+
+class DBManager: NSObject {
+
+}
