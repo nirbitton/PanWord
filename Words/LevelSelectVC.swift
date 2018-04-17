7b6b54a2322a08b78a27c694f562a0abcd71907e
Nir Bitton <nirbittone@gmail.com>
2018-04-17 22:50:25 +0300

    new levels, new words

--- Words/LevelSelectVC.swift
+++ Words/LevelSelectVC.swift
@@ -45,52 +45,72 @@ class LevelSelectVC: UIViewController, UICollectionViewDataSource {
         levelsArr.removeAll()
         let savedLevel = DBManager.getSavedLevel()
         let savedWord = DBManager.getSavedWord()
-        if savedLevel > 0 {
-            levelsArr.append(LevelSelectData(icon: "🐮", levelName: "חיות", nunOfWords: String(wordsArr[0].count) + "/" + String(wordsArr[0].count)))
+        
+        let animals = LevelSelectData(icon: "🐮", levelName: "חיות", nunOfWords: "")
+        self.addToLevelsArr(levelNumber: 0, levelSelectData: animals, savedLevel: savedLevel, savedWord: savedWord)
+        
+        let names = LevelSelectData(icon: "👶", levelName: "שמות", nunOfWords: "")
+        self.addToLevelsArr(levelNumber: 1, levelSelectData: names, savedLevel: savedLevel, savedWord: savedWord)
+        
+        let colors = LevelSelectData(icon: "🌈", levelName: "צבעים", nunOfWords: "")
+        self.addToLevelsArr(levelNumber: 2, levelSelectData: colors, savedLevel: savedLevel, savedWord: savedWord)
+        
+        let food = LevelSelectData(icon: "🍲", levelName: "מאכלים", nunOfWords: "")
+        self.addToLevelsArr(levelNumber: 3, levelSelectData: food, savedLevel: savedLevel, savedWord: savedWord)
+        
+        let body = LevelSelectData(icon: "👁️", levelName: "גוף האדם", nunOfWords: "")
+        self.addToLevelsArr(levelNumber: 4, levelSelectData: body, savedLevel: savedLevel, savedWord: savedWord)
+        
+        let countries = LevelSelectData(icon: "🗺️", levelName: "מדינות", nunOfWords: "0/10")
+        self.addToLevelsArr(levelNumber: 5, levelSelectData: countries, savedLevel: savedLevel, savedWord: savedWord)
+        
+        let bible = LevelSelectData(icon: "📖", levelName: "תנ״ך", nunOfWords: "0/10")
+        self.addToLevelsArr(levelNumber: 6, levelSelectData: bible, savedLevel: savedLevel, savedWord: savedWord)
+        
+        let professions = LevelSelectData(icon: "🎭", levelName: "מקצועות", nunOfWords: "0/10")
+        self.addToLevelsArr(levelNumber: 7, levelSelectData: professions, savedLevel: savedLevel, savedWord: savedWord)
+        
+        
+//        levelsArr.append(LevelSelectData(icon: "🎬", levelName: "סרטים", nunOfWords: "0/10"))
+//        levelsArr.append(LevelSelectData(icon: "📖", levelName: "תנ״ך", nunOfWords: "0/10"))
+//        levelsArr.append(LevelSelectData(icon: "🎙️", levelName: "מוזיקה", nunOfWords: "0/10"))
+//        levelsArr.append(LevelSelectData(icon: "🎭", levelName: "מקצועות", nunOfWords: "0/10"))
+        
+    }
+    
+    func addToLevelsArr(levelNumber:Int, levelSelectData:LevelSelectData, savedLevel:Int, savedWord:Int) {
+        
+        if savedLevel > levelNumber {
+            levelsArr.append(LevelSelectData(icon: levelSelectData.icon!, levelName: levelSelectData.levelName!, nunOfWords: String(wordsArr[levelNumber].count) + "/" + String(wordsArr[levelNumber].count)))
         }
-        else if savedLevel == 0 {
-            levelsArr.append(LevelSelectData(icon: "🐮", levelName: "חיות", nunOfWords: String(savedWord) + "/" + String(wordsArr[0].count)))
+        else if savedLevel == levelNumber {
+            levelsArr.append(LevelSelectData(icon: levelSelectData.icon!, levelName: levelSelectData.levelName!, nunOfWords: String(savedWord) + "/" + String(wordsArr[levelNumber].count)))
         }
         else {
-            levelsArr.append(LevelSelectData(icon: "🐮", levelName: "חיות", nunOfWords: "0/" + String(wordsArr[0].count)))
+            levelsArr.append(LevelSelectData(icon: levelSelectData.icon!, levelName: levelSelectData.levelName!, nunOfWords: "0/" + String(wordsArr[levelNumber].count)))
         }
-        
-        if savedLevel > 1 {
-            levelsArr.append(LevelSelectData(icon: "👶", levelName: "שמות", nunOfWords: String(wordsArr[1].count) + "/" + String(wordsArr[1].count)))
-        }
-        else if savedLevel == 1 {
-            levelsArr.append(LevelSelectData(icon: "👶", levelName: "שמות", nunOfWords: String(savedWord) + "/" + String(wordsArr[1].count)))
-        }
-        else {
-            levelsArr.append(LevelSelectData(icon: "👶", levelName: "שמות", nunOfWords: "0/" + String(wordsArr[1].count)))
-        }
-        
-        if savedLevel > 2 {
-            levelsArr.append(LevelSelectData(icon: "🍲", levelName: "מאכלים", nunOfWords: String(wordsArr[2].count) + "/" + String(wordsArr[2].count)))
-        }
-        else if savedLevel == 2{
-            levelsArr.append(LevelSelectData(icon: "🍲", levelName: "מאכלים", nunOfWords: String(savedWord) + "/" + String(wordsArr[2].count)))
-        }
-        else {
-            levelsArr.append(LevelSelectData(icon: "🍲", levelName: "מאכלים", nunOfWords: "0/" + String(wordsArr[2].count)))
-        }
-        
-        levelsArr.append(LevelSelectData(icon: "👁️", levelName: "גוף האדם", nunOfWords: "0/10"))
-        levelsArr.append(LevelSelectData(icon: "🗺️", levelName: "מדינות", nunOfWords: "0/10"))
-        levelsArr.append(LevelSelectData(icon: "🎬", levelName: "סרטים", nunOfWords: "0/10"))
-        levelsArr.append(LevelSelectData(icon: "📖", levelName: "תנ״ך", nunOfWords: "0/10"))
-        levelsArr.append(LevelSelectData(icon: "🎙️", levelName: "מוזיקה", nunOfWords: "0/10"))
-        levelsArr.append(LevelSelectData(icon: "🎭", levelName: "מקצועות", nunOfWords: "0/10"))
     }
     
     func initWords() {
         wordsArr.removeAll()
-        let animals = ["אריה","תנין","נמלה","זבוב","יתוש","לטאה","עכבר","חתול","שועל","חמור","פרפר"]
-        let names = ["ימית","לביא","אביה","דפנה","איתן","מאיר","בארי","יוסף","ברוך","אסתר","יקיר","הילה","תומר","רונן","בתיה","דרור","כרמל","ירדן","מרים","אמיר","שרון","מיקי","יאיר"]
-        let food = ["פיצה","במבה","ביצה","פסטה","חציל","גמבה","פיתה","ציפס","תפוח","חלבה","טוסט","מעדן","עוגה","כרוב"]
+        let animals = ["אריה","תנינ","נמלה","זבוב","יתוש","לטאה","עכבר","חתול","שועל","חמור","פרפר"]
+        let names = ["ימית","לביא","בארי","אביה","דפנה","איתנ","מאיר","יוספ","ברוכ","אסתר","יקיר","הילה","תומר","רוננ","בתיה","דרור","כרמל","ירדנ","מרימ","אמיר","שרונ","מיקי","יאיר"]
+        let colors = ["אדומ","צהוב","ירוק","כחול","שחור","כתומ","ורוד","תכלת","סגול","ציאנ","אפור"]
+        let food = ["פיצה","במבה","ביצה","פסטה","חציל","גמבה","פיתה","ציפס","תפוח","חלבה","טוסט","מעדנ","עוגה","כרוב"]
+        let body = ["אוזנ","מרפק","לשונ","אצבע","פנימ","טבור","זרוע","ריאה","בוהנ"]
+        let countries = ["גאנה","בליז","בנינ","הודו","יוונ","ירדנ","לאוס","ליטא","מאלי","מלטה","נפאל","סנגל","ספרד","פנמה","צרפת","קנדה","קניה","תימנ"]
+        let bible = ["תורה","שמות","הושע","יואל","בארי","עמוס","יונה","מיכה","נחומ","משלי","איוב","איכה","קהלת","אסתר","שלמה","עזרא","רבקה","פרעה","יצחק","נביא","עשיו","שאול","בעשא","אחאב","יעקב"]
+        let professions = ["רופא","מורה","מנהל","אופה","אספנ","בלדר","בלשנ","בנאי","במאי","שוטר","מציל","כבאי","זבלנ","ברמנ","גובה","חבלנ","מרגל","משרת","סוהר","ליצנ","חזאי","מאלפ","מדענ","חלבנ","גזבר","מלצר","מנחה","צורפ","פקיד","מנתח","יועצ","הגאי","יחצנ","מנקה","רוקח","קלדנ","ספרנ","פועל","מכשפ","חלפנ","סוכנ","חובש","סופר","קוסמ","רקדנ","ימאי","מעצב","חובל","ירקנ","סריס","שחקנ","תופר","מנצח","גולש","חקינ","שומר","משיט","כורת","חדשנ","חותר","חרזנ"]
+        
+        
         wordsArr.append(animals)
         wordsArr.append(names)
+        wordsArr.append(colors)
         wordsArr.append(food)
+        wordsArr.append(body)
+        wordsArr.append(countries)
+        wordsArr.append(bible)
+        wordsArr.append(professions)
     }
 
     // MARK: UICollectionViewDataSource
